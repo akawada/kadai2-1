23392c7583c43e1da3b60ea2372b6135d1f98918
akawada <atsushicco51@hotmail.com>
2016-10-15 00:25:58 +0900

    kadai2-1-new

--- kadai2_1.java
+++ kadai2_1.java
@@ -3,26 +3,17 @@ public class kadai2_1
 	  public static void main(String args[])
 	  {
 		    String kekka; 
-		    // 配列の宣言。
-		    int kuku[][] = new int[9][9];
 
 		    // 結果を配列に保存。
-		    for( int i  = 0; i < 9; i++ ) {
-				for( int j = 0;  j < 9; j++ ){
-		    		kuku[i][j] = (i + 1) * (j + 1);
-				}
-		    }
-
-		    // 結果を表示。
-		    for( int i = 0; i < 9; i++ ) {
-		      for( int j = 0; j < 9; j++ ) {
-		    	  kekka = String.valueOf(kuku[i][j]);
-		    	  if (kekka.length() == 1){
+		    for( int i=0; i<9; i++ ) {
+		      for( int j=0; j<9; j++ ){
+		    	  kekka = String.valueOf((i+1) * (j+1));
+		    	  if (kekka.length() ==1){
 		    		  kekka = " " + kekka ;
 		    	  }
 		    	  System.out.print(kekka + " ");
 		      }
-		      	  System.out.println(); 
+	      	  System.out.println(); 
 		    }
 	  }
 }
