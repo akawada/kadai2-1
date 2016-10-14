39ca5ce75f415d52b7be54cece4db7f0c1ffc25d
akawada <atsushicco51@hotmail.com>
2016-10-15 00:39:09 +0900

    kadai2-1-new

--- kadai2_1.java
+++ kadai2_1.java
@@ -6,6 +6,7 @@ public class kadai2_1
 		for( int i=0; i<9; i++ ) {
 			for( int j=0; j<9; j++ ){
 				kekka = String.valueOf((i+1) * (j+1));
+				// 1の位を合わせるための処理
 				if (kekka.length() ==1){
 					kekka = " " + kekka ;
 				}
