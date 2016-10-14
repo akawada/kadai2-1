3d9232a52fc3cd490e7523988f7395efa2558b9a
akawada <atsushicco51@hotmail.com>
2016-10-15 00:32:54 +0900

    kadai2-1-new

--- kadai2_1.java
+++ kadai2_1.java
@@ -1,19 +1,17 @@
 public class kadai2_1
 {
-	  public static void main(String args[])
-	  {
-		    String kekka; 
-
-		    // Œ‹‰Ê‚ğ”z—ñ‚É•Û‘¶B
-		    for( int i=0; i<9; i++ ) {
-		      for( int j=0; j<9; j++ ){
-		    	  kekka = String.valueOf((i+1) * (j+1));
-		    	  if (kekka.length() ==1){
-		    		  kekka = " " + kekka ;
-		    	  }
-		    	  System.out.print(kekka + " ");
-		      }
-	      	  System.out.println(); 
-		    }
-	  }
+	public static void main(String args[])
+	{
+		String kekka; 
+			for( int i=0; i<9; i++ ) {
+				for( int j=0; j<9; j++ ){
+					kekka = String.valueOf((i+1) * (j+1));
+					if (kekka.length() ==1){
+						kekka = " " + kekka ;
+					}
+					System.out.print(kekka + " ");
+				}
+				System.out.println(); 
+			}
+	}
 }
